20be8f3232d7c899f0a0e59b3b02ed7b5f7b0ced
Sanz <muhammadiksannnn1348@gmail.com>
2026-09-15 09:42:38 +0700

    update ukuran profil

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -106,7 +106,7 @@ const ProfileImage = memo(() => {
             
             {/* Image */}
             <img
-              src="/image.JPG"
+              src="/image.jpeg"
               alt="Profile"
               className="w-full h-full object-cover transition-all duration-700 group-hover:scale-110"
               loading="lazy"
